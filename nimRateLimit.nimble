6018cdbe72ffa8382e2df87c8536c99203dbaf62
David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>
2024-10-04 13:16:21 +0200

    init

---
+++ nimRateLimit.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "David Krause (enthus1ast)"
+description   = "Rate limit"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 2.0.8"
---
+++ src/nimRateLimit.nim
@@ -0,0 +1,76 @@
+import tables, times, locks, os
+
+type
+  Ident* = tuple[ressource, who: string]
+  RateLimiter = object
+    tab: CountTable[Ident]
+  ThreadOptions = object
+    prl: ptr RateLimiter
+    sleepTimeout: int
+    decrAmount: int
+
+var rlLock: Lock
+var dt: Thread[ThreadOptions]
+
+proc newRateLimiter*(): RateLimiter =
+ result = RateLimiter()
+
+proc add*(rl: var RateLimiter, ident: Ident) =
+  {.cast(gcsafe).}:
+    rlLock.withLock:
+      rl.tab.inc(ident)
+
+proc isAllowed*(rl: RateLimiter, ident: Ident, maxTries = 3): bool =
+  {.cast(gcsafe).}:
+    rlLock.withLock:
+      return rl.tab[ident] <= maxTries
+
+proc decr(rl: var RateLimiter, decrementAmount = -1 ) =
+  {.cast(gcsafe).}:
+    rlLock.withLock:
+      let ctab = rl.tab 
+      for key in ctab.keys:
+        rl.tab.inc(key, val = -1 * abs(decrementAmount) )
+        if rl.tab[key] <= 0:
+          when not defined(release):
+            echo "DEL: ", key
+          rl.tab.del(key)
+
+proc decrementerThread(to: ThreadOptions) {.thread.} =
+  while true:
+    when not defined(release):
+      echo ".", to.prl[]
+    sleep to.sleepTimeout
+    to.prl[].decr(to.decrAmount)
+
+proc startDecrementerThread*(rl: RateLimiter, sleepTimeout = 1_000, decrAmount = 1) =
+  createThread(dt, decrementerThread, ThreadOptions(prl: addr rl, sleepTimeout: sleepTimeout, decrAmount: decrAmount))
+
+when isMainModule:
+  var rl = RateLimiter()
+  let ident: Ident = ("/admin/login", "192.168.1.123")
+  let ident2: Ident = ("/admin/login", "141.168.1.123")
+  rl.startDecrementerThread()
+  rl.add ident
+  for idx in 0..10:
+    rl.add ident2
+  echo rl
+  rl.add ident
+  echo rl.isAllowed(ident)
+  rl.add ident
+  echo rl.isAllowed(ident)
+  rl.decr
+  rl.decr
+  rl.decr
+  rl.decr
+  rl.add ident
+
+  echo rl
+  import random
+  while true:
+    sleep (rand(3000))
+    rl.add ident
+    echo rl
+    echo rl.isAllowed(ident)
+
+
